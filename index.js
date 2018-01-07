f5d1146b92f2decb035ccded8ae7cde895879318
Basiru Danso <basirudns@gmail.com>
2018-01-07 01:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = "Susan"
+var name = "Joe"
 var height = 74
-var message = ` is ${Height} inches tall`
+var message = `${name} is ${height} inches tall`
 
-module.exports = {name, height, lessage}
+module.export = {name, height, message}
